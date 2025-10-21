ce463005d84ce9892a467a6d50020e8a2eef85a2
Madleyym <tryandaasu@gmail.com>
2025-10-22 01:07:41 +0700

    feat: add Web3Provider component for enhanced blockchain integration and theme support

---
+++ providers/Web3Provider.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import {
+  RainbowKitProvider,
+  darkTheme,
+  lightTheme,
+} from "@rainbow-me/rainbowkit";
+import { config } from "@/lib/wagmi";
+import { useEffect, useState } from "react";
+
+const queryClient = new QueryClient();
+
+export function Web3Provider({ children }: { children: React.ReactNode }) {
+  const [isDark, setIsDark] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+
+    const isDarkMode =
+      localStorage.getItem("color-mode") === "dark" ||
+      (!localStorage.getItem("color-mode") &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+    setIsDark(isDarkMode);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={isDark ? darkTheme() : lightTheme()}
+          modalSize="compact"
+        >
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
