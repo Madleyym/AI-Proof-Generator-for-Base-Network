31f5812dfef5314db9f4f680fc87790919b3827f
Madleyym <tryandaasu@gmail.com>
2025-10-22 01:07:20 +0700

    fix: disable reactStrictMode and update webpack configuration for asset handling

--- next.config.js
+++ next.config.js
@@ -1,11 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
+  output: "export",
+  reactStrictMode: false, 
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-}
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      os: false,
+      crypto: false,
+      pino: false,
+    };
 
-module.exports = nextConfig
+    config.module.rules.push({
+      test: /node_modules\/@metamask/,
+      type: "asset/source",
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
